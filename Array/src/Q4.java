020cd40f5437c301d3de589a51500e04c2aa2373
Akanksha <kudaleakanksha29@gmail.com>
2025-07-25 17:53:28 +0530

    From Java Array Questions

---
+++ Array/src/Q4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter ascii number ");
+            int ascii=sc.nextInt();
+            arr[i]=ascii;
+        }
+        for(int i=0;i<n;i++){
+            char ch=(char)arr[i];
+            System.out.print(ch+" ");
+        }
+    }
+}
